f7f5cca0c9ac7b524c3fc8df05d5e33861bee40f
kheinrich93 <konstantin.heinrich@yahoo.com>
2024-06-12 11:24:13 +0200

    Refactored to use pandas for CSV operations

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import csv
+import pandas as pd
 
-from src.helper import scrap_wiki_for_songnames, output_csv
-
+from src.helper import scrap_wiki_for_songnames
 
 # Fetch songlist from URL of Band from Wikipedia
 url = 'https://en.wikipedia.org/wiki/List_of_songs_recorded_by_Green_Day'
 
 df = scrap_wiki_for_songnames(url)
-output_csv(df, 'output/song_names.csv')
+df.to_csv('output/song_names.csv', index=False)
 
-# fetch lyrics of those songs
+# Fetch lyrics of those songs
+# load csv file 
+df = pd.read_csv('output/song_names.csv')
+print(df.head())
