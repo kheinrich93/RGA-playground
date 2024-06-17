a4f8bac42be5a6a14452687a4574fd4ae5670267
kheinrich93 <konstantin.heinrich@yahoo.com>
2024-06-17 12:21:01 +0200

    Fixed non-paginated GET

--- extract_songtexts.py
+++ extract_songtexts.py
@@ -3,52 +3,62 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-
-from src.helper import get_artist_id, get_access_token
+from src.webscrap_helper import get_artist_id, get_access_token
 
 # TODO: check for duplicates of songs and quantity
 
-
-access_token = get_access_token(filename = 'auth_tokens/genius.json')
+access_token = get_access_token(filename='auth_tokens/genius.json')
 
 # Construct the API endpoint to get songs by the artist
 artist_name = 'Green Day'
 artist_id = get_artist_id(artist_name, access_token)
 artist_songs_endpoint = f'https://api.genius.com/artists/{artist_id}/songs'
 
-# Make the GET request to get songs by the artist
-response = requests.get(artist_songs_endpoint, headers={'Authorization': 'Bearer ' + access_token})
+lyrics_store = {}
+missing_lyrics = {}
+page = 1  # Start with the first page
 
-lyrics_store = {} 
-# Check if the request was successful
-if response.status_code == 200:
-    # Process the response to get song titles
-    data = response.json()
-    songs = data['response']['songs']
-
-    # First n songs for debugging
-    #songs = songs[:10]
-
-    print(f"Found {len(songs)} songs")
-
-    # Extract lyric 'path' from songs 
-    song_lyric_urls = [f"https://genius.com{song['path']}" for song in songs]
+while True:
+    # Make the GET request to get songs by the artist, 50 per page is max
+    response = requests.get(artist_songs_endpoint, headers={'Authorization': 'Bearer ' + access_token}, params={'page': page, 'per_page': 50})
     
-
-    for song_lyric_url in tqdm(song_lyric_urls):
-        # Make a GET request to the song URL to scrape the lyrics
-        lyrics_response = requests.get(song_lyric_url)
-        if lyrics_response.status_code == 200:
-            soup = BeautifulSoup(lyrics_response.text, 'html.parser')
-            # Check if lyrics exist on the page
-            lyrics_div = soup.find('div', attrs={'data-lyrics-container': 'true'})
-            if lyrics_div:
-                # Extract and store lyrics
-                lyrics_store[song_lyric_url] = lyrics_div.get_text(separator='\n')
-else:
-    print('Error:', response.status_code)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Process the response to get song titles
+        data = response.json()
+        songs = data['response']['songs']
+        
+        if not songs:
+            break  # Exit the loop if no more songs are returned
+        
+        # Extract lyric 'path' from songs 
+        song_lyric_urls = [f"https://genius.com{song['path']}" for song in songs]
+        
+        for song_lyric_url in tqdm(song_lyric_urls, desc=f'Processing page {page}'):
+            # Make a GET request to the song URL to scrape the lyrics
+            lyrics_response = requests.get(song_lyric_url)
+            if lyrics_response.status_code == 200:
+                soup = BeautifulSoup(lyrics_response.text, 'html.parser')
+                # Check if lyrics exist on the page
+                lyrics_div = soup.find('div', attrs={'data-lyrics-container': 'true'})
+                if lyrics_div:
+                    # Extract and store lyrics
+                    lyrics_store[song_lyric_url] = lyrics_div.get_text(separator='\n')
+            else:
+                print(f'Error fetching lyrics for URL: {song_lyric_url}')
+                missing_lyrics[song_lyric_url] = lyrics_response.status_code
+        
+        # Check if there is a next page
+        next_page = data['response']['next_page']
+        if not next_page:
+            break  # Exit the loop if no next page is available
+        page += 1  # Move to the next page
+    else:
+        print('Error:', response.status_code)
+        break
 
 # save lyrics to a file
 with open('output/lyrics.json', 'w') as f:
     json.dump(lyrics_store, f)
 
+print(f"Successfully fetched lyrics for {len(lyrics_store)} songs.")
