88e138602a2373d05be33c3ab315696a3d310823
kheinrich93 <konstantin.heinrich@yahoo.com>
2024-06-24 12:14:52 +0200

    Add hybrid retrieval

--- LLM_pipeline.py
+++ LLM_pipeline.py
@@ -4,7 +4,9 @@ from typing import List
 from haystack import Document, Pipeline
 from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder, SentenceTransformersTextEmbedder
-from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+from haystack.components.retrievers.in_memory import InMemoryBM25Retriever, InMemoryEmbeddingRetriever
+from haystack.components.joiners import DocumentJoiner
+from haystack.components.rankers import TransformersSimilarityRanker
 from haystack.components.readers import ExtractiveReader
 from haystack.components.builders import PromptBuilder
 
@@ -14,13 +16,15 @@ from src.helper import load_json, get_api_token, print_pretty_results
 
 # TODO: 
 '''
-- check hybrid retrieval: https://haystack.deepset.ai/tutorials/33_hybrid_retrieval
+- improve pipeline building: https://haystack.deepset.ai/tutorials/29_serializing_pipelines
 - try other document stores -> DB!
+- create general output print
 '''
 
 class NLP_pipeline():
     def __init__(self) -> None:
-        pass
+        # Initialize the Document Store and its embedding
+        self.document_store = InMemoryDocumentStore()
 
     def load_data(self, json_path:str):
         # Load JSON data
@@ -35,8 +39,7 @@ class NLP_pipeline():
         # Create documents with correct format 
         documents = [Document(content=lyrics, meta={"title": title}) for title, lyrics in self.data.items()]
 
-        # Initialize the Document Store and its embedding
-        self.document_store = InMemoryDocumentStore()
+        # Prepare indexing pipeline
         doc_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
         doc_embedder.warm_up()
 
@@ -44,14 +47,19 @@ class NLP_pipeline():
         docs_with_embeddings = doc_embedder.run(documents)
         self.document_store.write_documents(docs_with_embeddings["documents"])
 
-        self.retriever = InMemoryEmbeddingRetriever(self.document_store)
+        self.bm25_retriever = InMemoryBM25Retriever(self.document_store)
+        self.embedding_retriever = InMemoryEmbeddingRetriever(self.document_store)
+
+        # for hybrid retrieval
+        self.document_joiner = DocumentJoiner()
+        self.ranker = TransformersSimilarityRanker(model="BAAI/bge-reranker-base")
 
     def create_text_embedder(self):
         # prompt/query embedding
         self.text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
 
     def create_prompt_builder(self):
-        # Create a prompt builder
+        # Create a custom prompt with prompt builder
         template = """
         Create song lyrics similar to {{question}} and use the following lyrics as context:
         {% for document in documents %}
@@ -64,6 +72,24 @@ class NLP_pipeline():
     def create_llm_generator(self):
         self.llm_generator = GroqGenerator(get_api_token("auth_tokens/groq.json"))
 
+    def create_hybrid_extractive_pipeline(self, plot_pipeline:bool = False):
+        # Create pipeline components
+        self.hybrid_extractive_qa_pipeline = Pipeline()
+        
+        self.hybrid_extractive_qa_pipeline.add_component(instance=self.text_embedder, name="text_embedder")
+        self.hybrid_extractive_qa_pipeline.add_component(instance=self.embedding_retriever , name="embedding_retriever")
+        self.hybrid_extractive_qa_pipeline.add_component(instance=self.bm25_retriever , name="bm25_retriever")
+        self.hybrid_extractive_qa_pipeline.add_component(instance=self.document_joiner, name="document_joiner")
+        self.hybrid_extractive_qa_pipeline.add_component(instance=self.ranker, name="ranker")
+
+        # Connect components
+        self.hybrid_extractive_qa_pipeline.connect("text_embedder", "embedding_retriever")
+        self.hybrid_extractive_qa_pipeline.connect("bm25_retriever", "document_joiner")
+        self.hybrid_extractive_qa_pipeline.connect("embedding_retriever", "document_joiner")
+        self.hybrid_extractive_qa_pipeline.connect("document_joiner", "ranker")
+
+        self.hybrid_extractive_qa_pipeline.draw("hybrid_extractive_qa_pipeline.png") if plot_pipeline else None
+
 
     def create_extractive_pipeline(self, plot_pipeline:bool = False):
         # Create reader for output
@@ -74,12 +100,12 @@ class NLP_pipeline():
         self.extractive_qa_pipeline = Pipeline()
         
         self.extractive_qa_pipeline.add_component(instance=self.text_embedder, name="text_embedder")
-        self.extractive_qa_pipeline.add_component(instance=self.retriever , name="retriever")
+        self.extractive_qa_pipeline.add_component(instance=self.embedding_retriever , name="embedding_retriever")
         self.extractive_qa_pipeline.add_component(instance=self.reader, name="reader")
 
         # Connect components
-        self.extractive_qa_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-        self.extractive_qa_pipeline.connect("retriever.documents", "reader.documents")
+        self.extractive_qa_pipeline.connect("text_embedder", "embedding_retriever")
+        self.extractive_qa_pipeline.connect("embedding_retriever.documents", "reader.documents")
 
         self.extractive_qa_pipeline.draw("extractive_qa_pipeline.png") if plot_pipeline else None
 
@@ -88,13 +114,13 @@ class NLP_pipeline():
         self.rag_pipeline = Pipeline()
 
         self.rag_pipeline.add_component(instance=self.text_embedder, name="text_embedder")
-        self.rag_pipeline.add_component(instance=self.retriever , name="retriever")
+        self.rag_pipeline.add_component(instance=self.embedding_retriever , name="embedding_retriever")
         self.rag_pipeline.add_component(instance=self.prompt_builder, name = "prompt_builder")
         self.rag_pipeline.add_component(instance=self.llm_generator, name="generator")
 
         # Connect components
-        self.rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-        self.rag_pipeline.connect("retriever", "prompt_builder.documents")
+        self.rag_pipeline.connect("text_embedder.embedding", "embedding_retriever.query_embedding")
+        self.rag_pipeline.connect("embedding_retriever", "prompt_builder.documents")
         self.rag_pipeline.connect("prompt_builder", "generator")
 
         self.rag_pipeline.draw("rag_pipeline.png") if plot_pipeline else None
@@ -105,8 +131,19 @@ class NLP_pipeline():
         for q in query:
             response = self.extractive_qa_pipeline.run(
                 data={"text_embedder": {"text": q},
-                                        "retriever": {"top_k": 3}, 
-                                        "reader": {"query": q, "top_k": 2}})
+                                        "reader": {"query": q, "top_k": 3}})
+
+            responses.append(response)
+
+        print(responses)
+
+
+    def run_hybrid_extractive_pipeline(self, query:List):
+        responses = []
+        for q in query:
+            response = self.hybrid_extractive_qa_pipeline.run(
+                                        {"text_embedder": {"text": q}, "bm25_retriever": {"query": q}, "ranker": {"query": q, "top_k": 3}})
+            
             responses.append(response)
 
         print(responses)
@@ -115,7 +152,7 @@ class NLP_pipeline():
         responses = []
         for q in query:
             response = self.rag_pipeline.run({"text_embedder": {"text": q}, 
-                                            "retriever": {"top_k": 3},
+                                            "embedding_retriever": {"top_k": 3},
                                             "prompt_builder": {"question": q}})
             responses.append(response)
 
@@ -133,6 +170,8 @@ NLP_pipeline.create_text_embedder()
 # --- qa pipeline ---
 # NLP_pipeline.create_extractive_pipeline()
 # NLP_pipeline.run_extractive_pipeline(query)
+# NLP_pipeline.create_hybrid_extractive_pipeline()
+# NLP_pipeline.run_hybrid_extractive_pipeline(query)
 
 # --- RAG pipeline ---
 # create prompt list that contains song with the following text around it "create song lyrics similar to [song]"
